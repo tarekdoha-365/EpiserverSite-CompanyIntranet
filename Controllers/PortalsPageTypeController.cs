04247d4bc1de86bc5f39e47dc792990f9dfcf07f
tarek doha <tarikdoha80@gmail.com>
2021-02-03 09:21:56 +0100

    Adding About and Portal Pages

---
+++ Controllers/PortalsPageTypeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EpiserverSite_CompanyIntranet.Controllers
+{
+    public class PortalsPageTypeController : Controller
+    {
+        // GET: PortalsPageType
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Pages/AboutPageType.cs
@@ -0,0 +1,23 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EpiserverSite_CompanyIntranet.Models.Pages
+{
+    [ContentType(DisplayName = "AboutPageType", GUID = "3fa70028-261b-4001-8989-62ccf03d5761", Description = "")]
+    public class AboutPageType : StartPageType
+    {
+        /*
+                [CultureSpecific]
+                [Display(
+                    Name = "Main body",
+                    Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual XhtmlString MainBody { get; set; }
+         */
+    }
+}
