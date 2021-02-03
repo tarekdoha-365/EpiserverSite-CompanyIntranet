a165bd99366bedcea442796fb5bf714a79a4661a
tarek doha <tarikdoha80@gmail.com>
2021-02-03 11:33:44 +0100

    update the Block and _Layout in Shared Folder

--- Models/Blocks/FeedsBlockType.cs
+++ Models/Blocks/FeedsBlockType.cs
@@ -2,8 +2,6 @@
 using EPiServer.Core;
 using EPiServer.DataAbstraction;
 using EPiServer.DataAnnotations;
-using EPiServer.Web;
-using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace EpiserverSite_CompanyIntranet.Models.Blocks
@@ -11,26 +9,44 @@ namespace EpiserverSite_CompanyIntranet.Models.Blocks
     [ContentType(DisplayName = "FeedsBlockType", GUID = "92b5c0a9-3e8e-4e86-9ce2-6e64d6343614", Description = "")]
     public class FeedsBlockType : BlockData
     {
-        [CultureSpecific]
         [Display(
-                 Name = "Heading",
-                 Description = "Add a heading.",
-                 GroupName = SystemTabNames.Content,
-                 Order = 1000)]
-        public virtual String Heading { get; set; }
-
-
-        [Display(
-            Name = "Image", Description = "Add an image (optional)",
+            Name = "Header",
+            Description = "Enter a header for the block",
             GroupName = SystemTabNames.Content,
-            Order = 1100)]
-        [UIHint(UIHint.Image)]
-        public virtual ContentReference Image1 { get; set; }
-        [Display(
-            Name = "Video", Description = "Add an image (optional)",
-            GroupName = SystemTabNames.Content,
-            Order = 1100)]
-        public virtual Url Video { get; set; }
+            Order = 1)]
+        [Required]
+        public virtual string Header { get; set; }
 
+        [Display(
+            Name = "Description",
+            Description = "Enter a Description for the block",
+            GroupName = SystemTabNames.Content,
+            Order = 2)]
+        [Required]
+        public virtual XhtmlString Description { get; set; }
+        [UIHint("Image")]
+        [Display(
+            Name = "Image",
+            Description = "Chose a image for the block",
+            GroupName = SystemTabNames.Content,
+            Order = 3)]
+        [Required]
+        public virtual Url Image { get; set; }
+
+        [Display(
+            Name = "Button label",
+            Description = "Enter a label for the button",
+            GroupName = SystemTabNames.Content,
+            Order = 4)]
+        [Required]
+        public virtual string Button { get; set; }
+
+        [Display(
+            Name = "Url",
+            Description = "Enter a Url for the button",
+            GroupName = SystemTabNames.Content,
+            Order = 5)]
+        [Required]
+        public virtual Url Url { get; set; }
     }
 }
